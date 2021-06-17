7682aaa860ad17adf34cb84c9ee0381e698c7abc
Pearl-web <pearlhamilton@hotmail.co.uk>
2021-06-17 16:20:48 +0100

    add style to counter

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -21,3 +21,18 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+div{
+  margin-bottom: 10px;
+}
+.counter{
+  font-size: 80px;
+}
+
+.buttons button{
+  font-size: 40px;
+  width: 100px;
+  margin: 0 10px;
+}
+</style>
