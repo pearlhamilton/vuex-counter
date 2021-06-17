b17c2b198f14b1aa4d25d5b4b97020aeb5bb7643
Pearl-web <pearlhamilton@hotmail.co.uk>
2021-06-17 16:52:46 +0100

    add notes on state, mutations, getters and modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,26 @@
 import { createStore } from 'vuex'
 
 export default createStore({
+  // state is where the data is stored
   state: {
   },
+  // mutations are methods that change the data that is in the state. 
+  // You can only change data in state by 'committing' or triggering a mutation
+  // You cannot trigger ascynchronous code inside a mutation
   mutations: {
   },
+  // Actions are also methods but cannot change data in state
+  //Actions commit a mutation which then changes data in state
+  // You can have asynchronous code in actions i.e. to get data from an API and wait for a response, then commit a mutation to update state 
+  // You dispatch an action
   actions: {
   },
+  // Getters allow us to get data from state
+  // You can access state directly from components however.
+  getters:{
+
+  },
+  // Allows us to break up our store into seperate modules with each module having its own state, mutations, actions and getters
   modules: {
   }
 })
